8de5bb002b1b5d67202e9e931f55d8a9b01ce351
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-04-14 16:57:53 -0500

    got initial forcast arrays loaded, made api call yay

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,65 @@
 var sample 
 /*
-  I am loading the sample data via another script tag on the index.html page, so I have that data 
-  available here as a global variable. It was named sample in the other file so we'll use that here.
+I am loading the sample data via another script tag on the index.html page, so I have that data 
+available here as a global variable. It was named sample in the other file so we'll use that here.
 */
 const APIKey = "b8fd273274a528d392087bd0cf821d1e";
 
+var searchButton = document.querySelector('.btn-block');
+
 var cityName = $('#search-input').val();
+
 var popularCityOpt = $('#dropdownMenuButton').val()
 
-var weatherFetchURLnumOne = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
+var weatherFetchURLnumOne = `https://api.openweathermap.org/data/2.5/forecast?q=Minneapolis&appid=${APIKey}`;
 
-var weatherFetchURLnumTwo = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+
+function makeAPICall(){
+  
+  fetch(weatherFetchURLnumOne)
+    .then(function (response) {
+      if(!response.ok){
+        throw response.json();
+      }
+      
+      return response.json();
+    })
+    .then( function (data) {
+      console.log(data);
+      var lat = data.city.coord.lat;
+      var long = data.city.coord.lon;
+
+      var weatherFetchURLnumTwo = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${APIKey}`;
+
+        fetch(weatherFetchURLnumTwo ) 
+          .then(function (response) {
+            return response.json();
+
+          })
+            .then(function (data) {
+              for( let i=0; i<40; i=i+8 ){
+
+                const daysInForecast = data.list 
+
+                const futureForecastArr = [] 
+
+                futureForecastArr.push( daysInForecast[i])
+
+                console.log(futureForecastArr)
+
+                const newForecastArr2 = data.list.filter( (_dayObj, idx) => idx % 8 === 0)
+                console.log(newForecastArr2)
+
+                return newForecastArr2
+
+              }
+
+        })
+    })
+
+}
 
 // This is the array of hour blocks: 8 per day, for a total of 40.
-const daysInForecast = sample.list 
 
 /*
 Each date object has a property called "dt", which is a Unix timestamp for the date and time 
@@ -21,15 +67,10 @@ of that object's data. The first one is 1681333200.
 */ 
 
 // Create a new array to hold one day block per forecast day.
-const newForecastArr = [] 
 
 // iterate over the 40 blocks, but we will do them 8 at a time, so that we get one per day.
-for( let i=0; i<40; i=i+8 ){
-  newForecastArr.push( sample.list[i])
-}
 
 // We now have a new array with one record for each day!
-console.log(newForecastArr)
 
 
 /* 
@@ -37,5 +78,9 @@ Want to see why arrow functions are cool? Combined with an array method you have
 yet, we can do all this work in one line of code. We will show you array.filter() later!
 */ 
 
-const newForecastArr2 = sample.list.filter( (_dayObj, idx) => idx % 8 === 0)
-console.log(newForecastArr2)
+searchButton.addEventListener('click', function(event){
+  event.preventDefault();
+  console.log(event.target);
+  makeAPICall()
+
+})
