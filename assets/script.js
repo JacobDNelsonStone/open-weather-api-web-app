ec4ae7537dfd13cde29ca6ab6140e537084f182c
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-04-17 10:22:14 -0500

    renested my code with help from joe to make functions work more independently

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var sample 
+var sample
 
 const APIKey = "b8fd273274a528d392087bd0cf821d1e";
 
@@ -26,37 +26,101 @@ var cityNameLocalStorageArray = []
 
 var recentCityDiv = $('#recentContainer');
 
-function setLocalStorage(){
+
+function setLocalStorage() {
   localStorage.setItem('recentCities', JSON.stringify(cityNameLocalStorageArray));
-    console.log(localStorage);
+  console.log(localStorage);
 }
 
 
-function getLocalStorage(){
-  cityButtonText = JSON.parse(localStorage.getItem('recentCities'))
-  
-  
-}
-// getLocalStorage()
+function getLocalStorage() {
+  cityNameLocalStorageArray = JSON.parse(localStorage.getItem('recentCities'))
 
-searchButton.click( function(event){
+
+}
+
+function makeRecentCityButtons(name) {
+
+  var localStorageCityButton = $(
+    `<button id="recent-cities" class="btn btn-info btn-block mt-1 pt-2 pb-3">${name}</button>
+  `);
+  recentCityDiv.append(localStorageCityButton);
+
+}
+
+function populateFutureForecast(futureForecastArray) {
+
+  for (var i = 0; i < futureForecastArray.length; i++) {
+    var futureForecastDay = futureForecastArray[i].dt_txt;
+    var FutureForecastIcon = futureForecastArray[i].weather[0].icon;
+    var weatherIcon = `https://openweathermap.org/img/wn/${FutureForecastIcon}@2x.png`
+    console.log(weatherIcon);
+    var FutureForecastWind = futureForecastArray[i].wind.speed;
+    var FutureForecastTemp = futureForecastArray[i].main.temp;
+    var FutureForecastHumid = futureForecastArray[i].main.humidity;
+    var FutureForecastDataEl = $(
+      `<div id="#forecastCard${[i]}" class="forecastCards bg-white">
+            <div id="#forecastInfo${[i]}" class="forecast-info">
+              <h5>${cityChoiceArray[0] + ' '}${futureForecastDay}</h5>
+              <img src="${weatherIcon}"</img><br>
+              <p>Temp: ${FutureForecastTemp}*F </p><br>
+              <p>Wind: ${FutureForecastWind} MPH</p><br>
+              <p>Humidity: ${FutureForecastHumid} %</p>
+            </div>
+          </div>
+          `);
+
+    console.log(FutureForecastCard);
+    console.log(FutureForecastDataEl)
+
+    FutureForecastCard.append(FutureForecastDataEl);
+  }
+}
+
+function populateCurrForecast(currForecast) {
+  var currForecastIcon = currForecast.weather[0].icon;
+  var weatherIcon = `https://openweathermap.org/img/wn/${currForecastIcon}@2x.png`
+  var currForecastWind = currForecast.wind.speed;
+  var currForecastTemp = currForecast.main.temp;
+  var currForecastHumid = currForecast.main.humidity;
+  var CurrForecastDataEl = $(
+    `<div id="CurrForecastCard" class="CurrForecastCard bg-white">
+    <div id="currForecastInfo" class="curr-forecast-info">
+      <h4>${cityChoiceArray[0] + " "}${currDayFormat}</h4>
+      <img src="${weatherIcon}"</img><br>
+      <p>Temp: ${currForecastTemp} *F</p><br>
+      <p>Wind: ${currForecastWind} MPH</p><br>
+      <p>Humidity: ${currForecastHumid} %</p>
+    </div>
+  </div>`);
+
+  console.log(currForecastCard);
+  //  currForecastCard.removeChildren()
+  //  currForecastCard.children().forEach(child => child.remove())
+  currForecastCard.append(CurrForecastDataEl);
+
+  makeRecentCityButtons(cityName)
+  setLocalStorage()
+}
+
+searchButton.click(function (event) {
   event.preventDefault();
   console.log(event.target);
   var cityTypefield = cityInputField.val();
-  
+
   cityChoiceArray.splice(0, 1, cityTypefield);
-  console.log(cityChoiceArray)  
+  console.log(cityChoiceArray)
   FutureForecastCard.innerHTML = '';
   currForecastCard.innerHTML = '';
   makeAPICall()
 
 })
 
-recentCityButton.click( function(e){
+recentCityButton.click(function (e) {
   e.preventDefault()
   console.log(e.target);
   var recentCity = recentCityButton.text();
-  
+
   cityChoiceArray.splice(0, 1, recentCity);
   console.log(cityChoiceArray)
   FutureForecastCard.outerHTML = '';
@@ -67,10 +131,10 @@ recentCityButton.click( function(e){
 })
 
 
-function makeAPICall(){
-  
-  
-  if(cityChoiceArray == null ){
+function makeAPICall() {
+
+
+  if (cityChoiceArray == null) {
     alert("You must select a city");
     location.reload()
   }
@@ -89,108 +153,50 @@ function makeAPICall(){
   //fetch function number 1 to get the 5 day forecast 
   fetch(weatherFetchURLnumOne)
     .then(function (response) {
-      
+
       return response.json();
     })
     // taking the json response and converting it into an array of objects 
-    .then( function (data) {
+    .then(function (data) {
       console.log(data);
-      
+
       getNumberOfDays(data)
-    function getNumberOfDays(){  // gary gave us this for loop to use 
-    for( let i=0; i<40; i=i+8 ){
+      function getNumberOfDays() {  // gary gave us this for loop to use 
+        for (let i = 0; i < 40; i = i + 8) {
 
-      const daysInForecast = data.list 
-      
-      futureForecastArray.push(daysInForecast[i])
+          const daysInForecast = data.list
 
-      
-    }
-    console.log(futureForecastArray)
-    
-  }
-  populateFutureForecast(futureForecastArray)
-})
-// for loop to populate the five day forecast
-function populateFutureForecast(futureForecastArray){ 
+          futureForecastArray.push(daysInForecast[i])
 
-    for(var i =0; i<futureForecastArray.length; i++){
-      var FutureForecastIcon = futureForecastArray[i].weather[0].icon;
-      var weatherIcon = `https://openweathermap.org/img/wn/${FutureForecastIcon}@2x.png`
-      console.log(weatherIcon);
-      var FutureForecastWind = futureForecastArray[i].wind.speed;
-      var FutureForecastTemp = futureForecastArray[i].main.temp;
-      var FutureForecastHumid = futureForecastArray[i].main.humidity;
-      var FutureForecastDataEl = $(       
-           `<div id="#forecastCard${[i]}" class="forecastCards bg-white">
-              <div id="#forecastInfo${[i]}" class="forecast-info">
-                <h5>${cityName + ' '}${currDayFormat}</h5>
-                <img src="${weatherIcon}"</img><br>
-                <p>Temp: ${FutureForecastTemp}*F </p><br>
-                <p>Wind: ${FutureForecastWind} MPH</p><br>
-                <p>Humidity: ${FutureForecastHumid} %</p>
-              </div>
-            </div>
-            `);
-      
-        console.log(FutureForecastCard);
-        console.log(FutureForecastDataEl)
-      
-        FutureForecastCard.append(FutureForecastDataEl);
-    }
+
+        }
+        console.log(futureForecastArray)
+
+      }
+      populateFutureForecast(futureForecastArray)
+    })
 
   var weatherFetchURLnumTwo = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
 
-  fetch(weatherFetchURLnumTwo ) 
+  fetch(weatherFetchURLnumTwo)
     .then(function (response) {
       return response.json();
-
     })
     // again taking the json and convefting it into array of objects, sepcifically for the current day forecast
     .then(function (data) {
-
       console.log(data)
-
       populateCurrForecast(data)
-        
-      
-    })
+  })
 
-    function populateCurrForecast(currForecast){
-      var currForecastIcon = currForecast.weather[0].icon;
-      var weatherIcon = `https://openweathermap.org/img/wn/${currForecastIcon}@2x.png`
-      var currForecastWind = currForecast.wind.speed;
-      var currForecastTemp = currForecast.main.temp;
-      var currForecastHumid = currForecast.main.humidity;
-      var CurrForecastDataEl = $(
-        `<div id="CurrForecastCard" class="CurrForecastCard bg-white">
-          <div id="currForecastInfo" class="curr-forecast-info">
-            <h4>${cityName + " "}${currDayFormat}</h4>
-            <img src="${weatherIcon}"</img><br>
-            <p>Temp: ${currForecastTemp} *F</p><br>
-            <p>Wind: ${currForecastWind} MPH</p><br>
-            <p>Humidity: ${currForecastHumid} %</p>
-          </div>
-        </div>`);
-
-        console.log(currForecastCard);
-
-      currForecastCard.append(CurrForecastDataEl);
-      
-      function makeRecentCityButtons(){
-        
-        var localStorageCityButton = $(
-          `<button id="recent-cities" class="btn btn-info btn-block mt-1 pt-2 pb-3">${cityName}</button>
-          `);
-          recentCityDiv.append(localStorageCityButton);
-        
-      }
-        makeRecentCityButtons()
-        setLocalStorage()
-    }
-    
-    
-  }
-  
 }
+
+// If I load the page and there's local storage...
+// I want to show all the searched items as buttons
+// I want to show the most recent search as data on the page
+
+// V V V V V PAGE JUST LOADED / RELOADED HERE AND NOW  V V V V V
+
 getLocalStorage()
+cityNameLocalStorageArray.forEach(city => makeRecentCityButtons(city))
+
+
